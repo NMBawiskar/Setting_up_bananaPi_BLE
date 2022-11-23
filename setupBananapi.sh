f5120e574ed56c80e61f7fd00f7c2990280b9751
NBawiskar <bawiskar.nilu@gmail.com>
2022-11-23 12:22:26 +0530

    psutil and path lib added, /var/dpkg/lock remove code added

--- setupBananapi.sh
+++ setupBananapi.sh
@@ -1,3 +1,9 @@
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
+
+sudo apt-get update
+
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
 cd /opt
@@ -19,7 +25,7 @@ sudo apt-get update -y
 sudo apt-get install -y freetds-dev
 sudo apt install unixodbc-dev
 pip3.8 install pymssql==2.2.5
-echo "=============================2.  pymssql Installation Done ========================================"
+echo "=============================3.  pymssql Installation Done ========================================"
 echo "Adding USER to dialout group for Serial communication......."
 sudo adduser $USER dialout 
 
@@ -30,11 +36,16 @@ sudo chown pi ttyS0
 echo "Changing python3.8 folder permission......."
 sudo chmod 777 -R /usr/local/bin/python3.8
 cd /home/pi/
+echo "=============================4.  Set permissions  Done ========================================"
 git clone https://github.com/LeMaker/RPi.GPIO_BP -b bananapro
 sudo apt-get update
 sudo apt-get install python-dev
 cd /home/pi/RPi.GPIO_BP
-python3.8 setup.py install                 
+python3.8 setup.py install     
+           
+echo "=============================5.  Rpi.GPIO_BP installation Done ========================================"
 sudo python3.8 setup.py install
-
-pip3.8 install psutils 
+pip3.8 install psutil==5.9.4
+pip3.8 install path==16.5.0
+echo "=============================6.  psutil and path libraries installation Done ========================================"
+echo "==================== INSTALLATION COMPLETE !!!!!!!! ==========================================="
