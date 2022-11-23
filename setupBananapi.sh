5af87b2b454aa9a61496c0420f8a41d1745f479c
NBawiskar <bawiskar.nilu@gmail.com>
2022-11-23 12:34:10 +0530

    added zlib lib

--- setupBananapi.sh
+++ setupBananapi.sh
@@ -6,6 +6,7 @@ sudo apt-get update
 
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
+sudo apt-get install zlib1g-dev
 cd /opt
 sudo wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
 sudo tar xzf Python-3.8.12.tgz
